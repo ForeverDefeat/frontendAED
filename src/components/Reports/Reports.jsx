b85615d2b3933c30031081c515dbf5f6a59312bd
ForeverDefeat <rodrigomp.3008@gmail.com>
2025-11-30 14:39:07 -0500

    Fix import path for api module in Reports.jsx

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import api from "../api/api";
+import api from "../../api/api";
 
 import Filters from "./Filters";
 import KPICards from "./KPICards";
